48bd14921fe3799777d5f6ea12fd11e30c8a7d04
Mike Jones <bristol.mike.jones@gmail.com>
2023-02-07 10:48:11 +0000

    revert file as liquibase needs it to remain consistent

--- sql/views.sql
+++ sql/views.sql
@@ -1,3 +1,25 @@
 /*
- This is intetionally blank.  The view is now created after the tables in 003_views.sql
+    This view retrieves all the latest events for a booking (DELETED: 0) where a VSA has been allocated
+    or the ATF has cancelled the event for billing purposes.
 */
+CREATE OR REPLACE VIEW daily_booking_alterations AS
+SELECT
+    s.C_ID,
+    st.STAFF_ID,
+    se.STATUS,
+    se.EVENT_DATE,
+    se.EVENT_START,
+    se.EVENT_END
+FROM EDH_WMS_STAGING.NGT_SITE_EVENTS se
+JOIN EDH_WMS_STAGING.NGT_SITE s
+    ON se.SITE_ID = s.SITE_ID
+JOIN EDH_WMS_STAGING.NGT_STAFF st
+    ON se.STAFF_ID = st.STAFF_ID
+WHERE (
+    (se.STATUS = 'ALLOCATED')
+    OR 
+    (se.CREATED BETWEEN DATE(se.EVENT_DATE - INTERVAL 6 DAY) AND DATE(se.EVENT_DATE)
+    AND se.STATUS = 'ATF CANCELLED')
+)
+AND se.DELETED = 0
+ORDER BY s.SITE_ID;
